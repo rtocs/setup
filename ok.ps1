5bf39c2ce78d0f8474000d96068dfc1de645edfc
sco <scottmathiasrichards@gmail.com>
2024-05-13 21:44:51 +0200

    add to path

--- ok.ps1
+++ ok.ps1
@@ -1,6 +1,13 @@
-
 winget install BurntSushi.ripgrep.MSVC
 winget install Neovim.Neovim
-
 Copy-Item .ideavimrc $env:USERPROFILE
 
+
+function add-to-path
+{
+	[Parameter()]
+	[string]$itemToAdd
+	!if ($env:Path -contains $itemToAdd){
+		$env:Path += ";" + $itemToAdd 
+	}
+}
